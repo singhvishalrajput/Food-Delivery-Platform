2635cfcd4378dbd0c432ca4c3482cb8daf3d6f38
Vishal Singh <singh287686@gmail.com>
2024-11-03 23:35:39 +0530

    Implemented the functionality of user authentication

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
 
 //app config
@@ -22,6 +23,9 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+
+app.use("/api/user",userRouter)
+
 
 app.get("/",(req, res)=>{
     res.send("API Working.")
