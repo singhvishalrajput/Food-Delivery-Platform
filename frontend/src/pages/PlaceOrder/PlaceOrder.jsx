37d9788f0f2f4ffa002d02cd92afcf04180bdd08
Vishal Singh <singh287686@gmail.com>
2024-11-06 19:47:30 +0530

    Added MyOrders page to see the successfull orders and their progress

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,30 +1,96 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./PlaceOrder.css"
-import { StoreContext } from '../../context/storeContext'
+import { StoreContext } from '../../context/StoreContext.jsx'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const PlaceOrder = () => {
 
-  const {getTotalCartAmount} = useContext(StoreContext)
+  const {getTotalCartAmount, token, food_list, cartItems, url} = useContext(StoreContext)
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName:"",
+    email:"",
+    street:"",
+    city:"",
+    state:"",
+    zipcode:"",
+    country:"",
+    phone:""
+  })
+
+  const onChangeHandler = (event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  
+  const placeOrder = async(event) =>{
+    event.preventDefault()
+    let orderItems = [];
+    food_list.map((item)=>{
+      if(cartItems[item._id]>0){
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id]
+        orderItems.push(itemInfo)
+      }
+    })
+    let orderData = {
+      address:data,
+      items:orderItems,
+      amount:getTotalCartAmount()+2,
+    }
+    try {
+      const response = await axios.post(url + "/api/order/place", orderData, { headers: { token } });
+      console.log(response.data); // Log the entire response for debugging
+      if (response.data.success) {
+        const { session_url } = response.data;
+        if (session_url) {
+          window.location.replace(session_url);
+        } else {
+          alert("Session URL not found. Please try again.");
+        }
+      } else {
+        alert("Error placing order. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error placing order:", error);
+      alert("An error occurred while placing the order.");
+    }
+  }
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(!token){
+      navigate("/cart")
+    }else if(getTotalCartAmount()===0){
+      navigate("/cart")
+    }
+
+  },[token])
 
   return (
-    <form className='place-order'>
+    <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
           <p className="title">Delivery Information</p>
           <div className="multi-fields">
-            <input type="text" placeholder='First name' />
-            <input type="text" placeholder='Last  name'/>
+            <input required  name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name' />
+            <input required name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last  name'/>
           </div>
-          <input type="email" placeholder='Email address' />
-          <input type="text" placeholder='Street' />
+          <input required name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder='Email address' />
+          <input required name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' />
           <div className="multi-fields">
-            <input type="text" placeholder='City' />
-            <input type="text" placeholder='State'/>
+            <input required name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
+            <input required name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State'/>
           </div>
           <div className="multi-fields">
-            <input type="text" placeholder='Zip code' />
-            <input type="text" placeholder='Country'/>
+            <input required name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code' />
+            <input required name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country'/>
           </div>
-          <input type="text" placeholder='Phone' />
+          <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone' />
       </div>
       <div className="place-order-right">
           <div className="cart-total">
@@ -37,7 +103,7 @@ const PlaceOrder = () => {
                       <hr />
                       <div className="cart-total-details">
                           <p>Delivery Fee</p>
-                          <p>${getTotalCartAmount()+2===2? 0: getTotalCartAmount()+2}</p>
+                          <p>${getTotalCartAmount()+2===2? 0:2}</p>
                       </div>
                       <hr />
                       <div className="cart-total-details">
@@ -46,7 +112,7 @@ const PlaceOrder = () => {
                       </div>
                       
                   </div>
-                  <button >PROCEED TO PAYMENT</button>
+                  <button type='submit' >PROCEED TO PAYMENT</button>
               </div>
       </div>
     </form>
